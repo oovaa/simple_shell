9b7b821f6e007f2edbfd5dc62110f955b3058430
oovaa <omarvx211@gmail.com>
2023-10-15 15:24:03 +0200

    whaaaaaaaaayyyyyy at 15-15

--- start.c
+++ start.c
@@ -6,13 +6,38 @@
  *
  */
 
-int main(int argc, char **argv) {
-    if (isatty(STDIN_FILENO))
-	{
-		return interactive_mode();
-	}
-	else
-	{
-		return non_interactive_mode(argc, argv);
-	}
+int main(void) {
+    char *command = NULL; 
+    char **tokcom = NULL; 
+    size_t line = 0; 
+    int re = 0; 
+    char *path_command;
+    _setenv("mash", _getenv("SHELL"), 1); 
+
+    while (1) 
+    { 
+        _puts("$ "); 
+        getline(&command, &line, stdin); 
+
+        append_text_to_file("history.txt", command); 
+
+        command[_strcspn(command, '\n')] = '\0'; 
+
+        replace_variables(command, re); 
+
+        command = clean(command); 
+
+        if (command[0] == '\0') 
+            continue; 
+
+        tokcom = strtoarr(command, ' '); 
+
+        path_command = look_in_path(tokcom[0]); 
+
+        re = exe(path_command, tokcom); 
+    } 
+    free(command);
+    free(path_command);
+    free_strarr(tokcom);
+    return re; 
 }
